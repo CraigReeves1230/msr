5fbbd372dbef4a505a5724b36e0a9a9ae9ec2ade
CraigReeves1230 <reevescd1@gmail.com>
2016-05-31 20:52:14 -0400

    changed wrestler rating algorithm

--- app/models/wrestler.rb
+++ app/models/wrestler.rb
@@ -8,11 +8,17 @@ class Wrestler < ActiveRecord::Base
     # average scores together with the same principles from match star rater but slightly different
     score = (execution + (ability * 3.0) + (psychology * 3.0)) / 7.0
 
-    # reward wrestler if execution and ability is strong
-    score += 0.25 if execution && ability >= 3.5
+    # reward wrestler if all three traits are strong
+    score += 0.25 if execution >= 3.25 && ability >= 3.25 && psychology >= 3.25
 
-    # if the wrestler's execution is exemplary, reward wrestler only if other traits are strong.
-    score += 0.25 if execution > 3.5 && psychology >= 3.25 && ability >= 3.25
+    #reward wrestler again if ability and execution are strong
+    score += 0.25 if execution >= 3.25 && ability >= 3.25
+
+    #reward wrestler again if psychology and execution are exceptional
+    score += 0.25 if execution >= 3.50 && psychology >= 3.50
+
+    #reward wrestler again if all three traits are exceptional. This is an exceptional worker
+    score += 0.25 if execution >= 3.50 && psychology >= 3.50 && ability >= 3.50
 
     #return score
     score
